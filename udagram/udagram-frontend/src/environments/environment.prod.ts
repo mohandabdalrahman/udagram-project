9bb1d81a054d24b12fb9adf62c6245b1c496af99
Mohand_abdalrahman <mohand_abdrazek@siliconexpert.com>
2022-10-28 17:08:37 +0200

    edit deploy script

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost:'http://udagram-env.eba-m9rqikpr.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-env.eba-m9rqikpr.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost:'http://udagram-env.eba-m9rqikpr.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-env.eba-m9rqikpr.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
